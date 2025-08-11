40efccd204ea528710e6457e714b5047e616adda
devpedrosena1 <devpedrosena1@gmail.com>
2025-08-11 08:44:58 -0300

    criando projeto com POO

---
+++ 2tdsqProjetos/2_OOP/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using _2_OOP.entities;
+
+Console.WriteLine("Hello, World!");
+
+var novaPessoa = new Pessoa();
---
+++ 2tdsqProjetos/2_OOP/entities/Pessoa.cs
@@ -0,0 +1,28 @@
+﻿namespace _2_OOP.entities;
+
+// a declaração de uma classe é o padrão {acesso} class {NomeDaClasse}
+public class Pessoa
+{
+
+    public string nome { get; set; }
+    public int idade { get; set; }
+    public string email { get; set; }
+
+    // ctor -> atalho para escrever construtor vazio
+    public Pessoa()
+    {
+        
+    }
+
+    // ctorp -> atalho para escrever construtor cheio
+    public Pessoa(string nome, int idade, string email)
+    {
+        this.nome = nome;
+        this.idade = idade;
+        this.email = email;
+    }
+
+    public string ExibirDetalhes() => $"Nome: {nome}, idade: {idade}, Email: {email}";
+    
+
+}
