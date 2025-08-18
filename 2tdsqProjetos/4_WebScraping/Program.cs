f2cf13443174a41cbdaf71de41749d4ca238a4b8
devpedrosena1 <devpedrosena1@gmail.com>
2025-08-18 09:18:29 -0300

    entendendo como funciona o scraping, utilizando foreach, parallel a fim de diminuir o tempo de demanda e colocando delay nas tasks

--- 2tdsqProjetos/4_WebScraping/Program.cs
+++ 2tdsqProjetos/4_WebScraping/Program.cs
@@ -4,30 +4,60 @@ using HtmlAgilityPack;
 
 Console.WriteLine("Hello, World!");
 
+// Carrega o HTML da página
 var htmlUrl = "https://www.metacritic.com/game/donkey-kong-bananza/";
 
-var web = new HtmlWeb();
-var htmlDoc = web.Load(htmlUrl);
-var title = htmlDoc.DocumentNode.SelectSingleNode("//h1")?.InnerText;
+var games = new List<MetacriticGame>();
 
-// Take structure website
-// Console.WriteLine(htmlDoc.DocumentNode.InnerHtml);
+var urlList = new[]
+{
+    "https://www.metacritic.com/game/donkey-kong-bananza",
+    "https://www.metacritic.com/game/clair-obscur-expedition-33/"
+};
 
-// Console.WriteLine(title);
+Parallel.ForEach(urlList, htmlUrl =>
+{
+    games.Add(ScrapGame(htmlUrl).Result);
+});
 
-var metaScore = htmlDoc.DocumentNode.SelectSingleNode(
-    "/html/body/div[1]/div/div/div[2]/div[1]/div[1]/div/div/div[2]/div[3]/div[4]/div[1]/div/div[1]/div[2]/div/div/span"
+while(games.Count < urlList.Length)
+    // Aguarda até que todas as tarefas sejam concluídas
+    await Task.Delay(100);
+
+// foreach (var game  in urlList)
+// {
+//     games.Add(await ScrapGame(game));
+// }
+
+games.Add(await ScrapGame(htmlUrl));
+
+async Task<MetacriticGame> ScrapGame(string url)
+{
+    var web = new HtmlWeb();
+    var htmlDoc = await web.LoadFromWebAsync(htmlUrl);
+    var title = htmlDoc.DocumentNode.SelectSingleNode("//h1")?.InnerText;
+
+    // Take structure website
+    // Console.WriteLine(htmlDoc.DocumentNode.InnerHtml);
+
+    // Console.WriteLine(title);
+
+    var metaScore = htmlDoc.DocumentNode.SelectSingleNode(
+        "/html/body/div[1]/div/div/div[2]/div[1]/div[1]/div/div/div[2]/div[3]/div[4]/div[1]/div/div[1]/div[2]/div/div/span"
     )?.InnerText;
 
-// Console.WriteLine(metaScore);
+    // Console.WriteLine(metaScore);
 
-var userScore = htmlDoc.DocumentNode.SelectSingleNode(
-    "/html/body/div[1]/div/div/div[2]/div[1]/div[1]/div/div/div[2]/div[3]/div[4]/div[2]/div[1]/div[2]/div/div/span"
+    var userScore = htmlDoc.DocumentNode.SelectSingleNode(
+        "/html/body/div[1]/div/div/div[2]/div[1]/div[1]/div/div/div[2]/div[3]/div[4]/div[2]/div[1]/div[2]/div/div/span"
     )?.InnerText;
 
-// Console.WriteLine(userScore);
+    // Console.WriteLine(userScore);
 
-var metaCriticGame = new MetacriticGame(title, metaScore, userScore);
-Console.WriteLine(metaCriticGame.ToString());
+    var metaCriticGame = new MetacriticGame(title, metaScore, userScore);
+    Console.WriteLine(metaCriticGame.ToString());
+
+    return  metaCriticGame;
+}
 
 public record MetacriticGame(string? Title, string? MetaScore, string? UserScore);
